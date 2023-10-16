9f47b5be488d4645bb0ef124926b1c6337ebd07f
lesikalex905 <148013232+lesikalex905@users.noreply.github.com>
2023-10-16 09:46:47 -0400

    Add files via upload

---
+++ Refference file.R
@@ -0,0 +1,11 @@
+
+email = 'alexey.lesik@valtech.com'
+start_date <- '2023-09-01'
+end_date   <- '2023-09-30'
+property <- c(372803280, 385325923)
+path <- "C:\\Users\\alexey.lesik\\Documents\\Test\\"
+
+#Executive script
+source("C:\\Users\\alexey.lesik\\Documents\\RScripts\\GA4 data extraction\\GA4_Extraction.R")
+
+
