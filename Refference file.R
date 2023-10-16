923092da802b2f281d8ded5a4c80e7409b8c4763
lesikalex905 <148013232+lesikalex905@users.noreply.github.com>
2023-10-16 09:49:03 -0400

    Update Refference file.R

--- Refference file.R
+++ Refference file.R
@@ -1,11 +1,9 @@
 
-email = 'alexey.lesik@valtech.com'
+email = 'example@example.com'
 start_date <- '2023-09-01'
 end_date   <- '2023-09-30'
-property <- c(372803280, 385325923)
-path <- "C:\\Users\\alexey.lesik\\Documents\\Test\\"
+property <- c(property_id1, property_id2)
+path <- "C:\\Users\\Documents\\"
 
 #Executive script
-source("C:\\Users\\alexey.lesik\\Documents\\RScripts\\GA4 data extraction\\GA4_Extraction.R")
-
-
+source("C:\\Users\\Documents\\RScripts\\GA4 data extraction\\GA4_Extraction.R")
